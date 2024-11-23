9701098f6f072b2a08ac6874c01373a07a2b0892
滑墙 <whq20011120@hotmail.com>
2024-11-23 19:03:22 +0800

    Update converter.js

--- converter.js
+++ converter.js
@@ -68,8 +68,8 @@ function uploadAndConvert() {
 }
 function downloadResult() {
     const outputText = document.getElementById('outputText').value;
-    const fileType = /\{.*?\}/.test(outputText) ? 'application/json' :
-    /\<.*?\>/.test(outputText.replace(/\s/g, '')) ? 'application/xml' :
+    const fileType = outputText.startsWith('{') ? 'application/json' :
+    outputText.startsWith('<') ? 'application/xml' :
     'text/plain';
     const blob = new Blob([outputText], { type: fileType });
     const downloadLink = document.createElement('a');
